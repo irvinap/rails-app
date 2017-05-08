f478a8ccbf53dc9acc6bfc8baf42ced6dba43a53
Irvina Paul <irvinaivy@hotmail.com>
2017-05-08 20:23:39 +1000

    UPDATE: login page logout flash message

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+before_action :is_logged_in, only: [:contact]
+
   def home
   end
 
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,30 @@
+module SessionsHelper
+#logs in given user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  #returns current logged in user (if any)
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  #returns true if the user is logged in, false otherwise
+  def logged_in?
+      !current_user.nil?
+  end
+
+  #logs out current user
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  #checks if the user is logged in, otherwise redirect to the Homepage
+  def is_logged_in
+    if !logged_in?
+      flash[:notice] = "You must be logged in to access this function."
+      redirect_to root_path
+    end
+  end
+end
